29b6b6ee6728c805aee2714ec4c1c53d81bc6e4b
Hiromasa Ihara <iharahiromasa@gmail.com>
2025-01-07 17:37:04 +0900

    [from now] 2025/01/07 17:37:04
     selfdrive/car/honda/interface.py | 3 ++-
     1 file changed, 2 insertions(+), 1 deletion(-)

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -76,10 +76,11 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.pid.kf = 0.00006  # conservative feed-forward
 
     if candidate in HONDA_BOSCH:
+      ret.longitudinalTuning.kiBP = [0., 5., 35.]
+      ret.longitudinalTuning.kiV = [1.2, 0.8, 0.5]
       ret.longitudinalActuatorDelay = 0.5 # s
       if candidate in HONDA_BOSCH_RADARLESS:
         ret.stopAccel = CarControllerParams.BOSCH_ACCEL_MIN  # stock uses -4.0 m/s^2 once stopped but limited by safety model
-      ret.longitudinalTuning.kpV = [0.025]
     else:
       # default longitudinal tuning for all hondas
       ret.longitudinalTuning.kiBP = [0., 5., 35.]
